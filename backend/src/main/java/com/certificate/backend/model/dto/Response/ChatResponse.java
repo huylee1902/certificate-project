62a64d49e61f314ac464a1e6934f8f1ce60d40d3
freelancer761 <nguyenduyhoangz2005@gmail.com>
2026-05-22 10:23:32 +0700

    Add ChatResponse DTO for chat replies

---
+++ backend/src/main/java/com/certificate/backend/model/dto/Response/ChatResponse.java
@@ -0,0 +1,8 @@
+package com.certificate.backend.model.dto.Response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class ChatResponse {
+    private String reply;
+}
